d4901d56881805ebe24f81720f9b4153a9f53312
hosseindbk <hosseindbk@gmail.com>
2022-01-16 19:28:43 +0330

    v4.2 routeApi

--- routes/api.php
+++ routes/api.php
@@ -13,7 +13,25 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
-
+Route::group(['namespace' => 'Site'] , function () {
+    Route::get('/', 'IndexController@index')->name('/');
+});
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::group(['namespace' => 'Auth'] , function (){
+    // Authentication Routes...
+    Route::get('login'      , 'LoginController@showLoginuserForm')->name('loginuser');
+    Route::get('remember'   , 'LoginController@showLoginrememberForm')->name('remember');
+    Route::post('login-user', 'LoginController@loginuser')->name('login-user');
+    Route::get('logout'     , 'LoginController@logout')->name('logout');
+
+    // Registration Routes...
+    Route::get('register'   , 'RegisterController@showRegistrationuserForm');
+    Route::post('register'  , 'RegisterController@registeruser')->name('register');
+    Route::get('token'      , 'TokenController@showToken')->name('phone.token');
+    Route::post('token'     , 'TokenController@token')->name('verify.phone.token');
+    Route::get('welcome'    , 'WelcomeController@index' )->name('welcome');
+
 });
