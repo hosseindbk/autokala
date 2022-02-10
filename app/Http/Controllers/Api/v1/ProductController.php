2c76ece78211b516194b8b78286664dccfe92d63
hosseindbk <hosseindbk@gmail.com>
2022-02-10 18:42:07 +0330

    4.7 update after restore

--- app/Http/Controllers/Api/v1/ProductController.php
+++ app/Http/Controllers/Api/v1/ProductController.php
@@ -5,7 +5,6 @@ namespace App\Http\Controllers\Api\v1;
 use App\comment;
 use App\commentrate;
 use App\Http\Controllers\Controller;
-use App\Menu;
 use App\Product;
 use App\Product_group;
 use Illuminate\Support\Facades\DB;
@@ -35,23 +34,27 @@ class ProductController extends Controller
             ->toArray();
 
         $product_id       = Product::whereSlug($slug)->pluck('id');
+        $kala_group_id       = Product::whereSlug($slug)->pluck('kala_group_id');
 
         $commentratecount       = commentrate::whereCommentable_type('App\Product')->where('Commentable_id' ,$product_id)->whereApproved(1)->count();
         $comments               = comment::whereCommentable_type('App\Product')->whereIn('Commentable_id'   ,$product_id)->select('phone' , 'comment')->whereApproved(1)->latest()->get();
 
 
-        $productgroups = DB::table('car_products')
+        $cars = DB::table('car_products')
             ->leftJoin('car_brands', 'car_brands.id', '=', 'car_products.car_brand_id')
             ->leftJoin('car_models', 'car_models.id', '=', 'car_products.car_model_id')
             ->select('car_brands.title_fa as brand_title' , 'car_models.title_fa as model_title' , 'car_products.product_id')
             ->whereIn('product_id'  , $product_id)
             ->get();
 
+        $productgroups = Product_group::select('title_fa as title')->whereId($kala_group_id)->get()->toArray();
+
         $response = [
             'products'          =>  $products,
-            'productgroups'     =>  $productgroups,
+            'cars'              =>  $cars,
             'comment'           => $comments,
-            'commentratecount'  => $commentratecount
+            'commentratecount'  => $commentratecount,
+            'productgroup'      => $productgroups
 
         ];
         return Response::json(['ok' =>true ,'message' => 'success','response'=>$response]);
