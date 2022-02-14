c5077e78e41711605b41afed15527f55d0220c5f
hosseindbk <hosseindbk@gmail.com>
2022-02-14 21:23:04 +0330

    4.7 api

--- app/Http/Controllers/Api/v1/ProductController.php
+++ app/Http/Controllers/Api/v1/ProductController.php
@@ -30,10 +30,7 @@ class ProductController extends Controller
         $product_id       = Product::whereSlug($slug)->pluck('id');
 
         $commentratecount       = commentrate::whereCommentable_type('App\Product')->where('Commentable_id' ,$product_id)->whereApproved(1)->count();
-        $comments               = comment::whereCommentable_type('App\Product')->whereIn('Commentable_id'   ,$product_id)->select('phone' , 'comment' , 'id as comment_id')->whereParent_id(0)->whereApproved(1)->latest()->get();
 
-
-        $subcomments            = comment::whereCommentable_type('App\Product')->whereIn('Commentable_id'   ,$product_id)->select('phone' , 'comment' , 'parent_id')->where('parent_id' , '>', 0)->whereApproved(1)->latest()->get();
 
         $cars = DB::table('car_products')
             ->leftJoin('car_brands', 'car_brands.id', '=', 'car_products.car_brand_id')
@@ -44,20 +41,43 @@ class ProductController extends Controller
 
         $products = DB::table('products')
             ->leftJoin('product_groups', 'product_groups.id', '=', 'products.kala_group_id')
-            ->select('products.unicode' , 'products.slug' , 'products.image' , 'products.title_fa' , 'products.title_en' ,
-                'products.title_bazar_fa' , 'products.code_fani_company as company_code' , 'products.description' , 'product_groups.title_fa as productgroup')
+            ->select('products.unicode as unicode' , 'products.slug as slug' , 'products.image as image' , 'products.title_fa as title' , 'products.title_en as title_en' ,
+                'products.title_bazar_fa as title_bazar' , 'products.code_fani_company as company_code' , 'products.description as description' , 'product_groups.title_fa as productgroup'
+            ,'products.created_at as created_at')
             ->whereSlug($slug)
-            ->first();
-        $tmp=json_decode(json_encode($products), true);;
-        $medias                 = Media::select('image')->whereProduct_id($product_id)->get();
-        $medias = [$medias[0]['image']];
-        $tmp['p-image']=$medias;
+            ->get();
+        foreach ($products as $product) {
+
+            $test = [
+                'unicode'       => $product->unicode,
+                'slug'          => $product->slug,
+                'image'         => $product->image,
+                'title'         => $product->title,
+                'title_en'      => $product->title_en,
+                'title_bazar'   => $product->title_bazar,
+                'company_code'  => $product->company_code,
+                'description'   => $product->description,
+                'productgroup'  => $product->productgroup,
+                'created_at'    => jdate($product->created_at)->ago(),
+            ];
+        }
+        $tmp        = json_decode(json_encode($test), true);
+        $medias     = Media::select('image')->whereIn('product_id' , $product_id)->get();
+
+        foreach ($medias as $media){
+            $medis[]  =  $media->image;
+        }
+        $tmp['product-image']=$medis;
+
+        $comments               = comment::whereCommentable_type('App\Product')->whereIn('Commentable_id'   ,$product_id)->select('phone' , 'comment' , 'id as comment_id' , 'created_at')->whereParent_id(0)->whereApproved(1)->latest()->get();
+        $subcomments            = comment::whereCommentable_type('App\Product')->whereIn('Commentable_id'   ,$product_id)->select('phone' , 'comment' , 'parent_id')->where('parent_id' ,'>' ,  0)->whereApproved(1)->latest()->get();
+
 
         $response = [
             'products'          => $tmp,
             'cars'              => $cars,
             'comment'           => $comments,
-            'subcomment'        => $subcomments,
+//            'subcomment'        => $subcomments,
             'commentratecount'  => $commentratecount,
         ];
         return Response::json(['ok' =>true ,'message' => 'success','response'=>$response ]);
