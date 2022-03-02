9cb1f7dd573263f5f6f238d453d25e616d1e1cad
hosseindbk <hosseindbk@gmail.com>
2022-03-02 16:16:25 +0330

    4.8 api index

--- app/Http/Controllers/Api/v1/IndexController.php
+++ app/Http/Controllers/Api/v1/IndexController.php
@@ -3,12 +3,9 @@
 namespace App\Http\Controllers\Api\v1;
 
 use App\Brand;
-use App\City;
 use App\Http\Controllers\Controller;
-use App\Menu;
 use App\Offer;
 use App\Slide;
-use App\State;
 use App\Supplier;
 use App\Technical_unit;
 use Illuminate\Support\Facades\Response;
@@ -17,22 +14,22 @@ class IndexController extends Controller
 {
     public function index(){
 
-        $citis              = City::select('id as city_id','title as city' , 'state_id')->get()->toArray();
-        $state              = State::select('id as state_id','title as state')->get()->toArray();
-        $offers             = Offer::select('slug' , 'unicode_product' , 'supplier_id' , 'title_offer as title' , 'buyorsell' , 'single_price' , 'image1')->whereStatus(4)->whereHomeshow(1)->inRandomOrder()->get()->toArray();
-        $brands             = Brand::select('title_fa as title' , 'slug' , 'image')->whereStatus(4)->whereHomeshow(1)->inRandomOrder()->get()->toArray();
-        $technicalunits     = Technical_unit::select('title' , 'slug' , 'image')->whereStatus(4)->whereHomeshow(1)->inRandomOrder()->get()->toArray();
-        $suppliers          = Supplier::select('title' , 'slug' , 'image')->whereStatus(4)->whereHomeshow(1)->inRandomOrder()->get()->toArray();
-        $orginal_slides     = Slide::select('image')->whereStatus(4)->wherePosition(1)->latest()->get()->toArray();
+        $offers             = Offer::select('slug' , 'unicode_product' , 'supplier_id' , 'title_offer as title' , 'buyorsell' , 'single_price' , 'image1')->whereStatus(4)->whereHomeshow(1)->inRandomOrder()->get();
+        $brands             = Brand::select('title_fa as title' , 'slug' , 'image')->whereStatus(4)->whereHomeshow(1)->inRandomOrder()->get();
+        $technicalunits     = Technical_unit::select('title' , 'slug' , 'image')->whereStatus(4)->whereHomeshow(1)->inRandomOrder()->get();
+        $suppliers          = Supplier::select('title' , 'slug' , 'image')->whereStatus(4)->whereHomeshow(1)->inRandomOrder()->get();
+        $orginal_slides     = Slide::select('image as images')->whereStatus(4)->wherePosition(1)->latest()->get();
+
+        foreach($orginal_slides as $orginal_slide) {
+            $slide [] = $orginal_slide->images;
+        }
 
         $response = [
-                'citis'         => $citis ,
-                'state'         =>$state ,
-                'offer'         =>$offers ,
-                'brand'         =>$brands,
-                'technicalunits'=>$technicalunits  ,
-                'suppliers'     =>$suppliers ,
-                'orginal_slides'=>$orginal_slides,
+                'offer'          => $offers ,
+                'brand'          => $brands,
+                'technicalunits' => $technicalunits  ,
+                'suppliers'      => $suppliers ,
+                'orginal_slides' => $slide,
         ];
         return Response::json(['ok' =>true ,'message' => 'success','response'=>$response]);
 
