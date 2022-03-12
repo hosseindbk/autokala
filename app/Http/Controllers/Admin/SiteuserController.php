46c8f4ca6de981e188f34a92189fb3678b37caf2
divanbeigi <hosseindbk@yahoo.com>
2022-03-12 15:37:55 +0330

    autokala v4.8  user datatable

--- app/Http/Controllers/Admin/SiteuserController.php
+++ app/Http/Controllers/Admin/SiteuserController.php
@@ -8,23 +8,64 @@ use App\Submenudashboard;
 use App\Type_user;
 use App\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
+use Yajra\DataTables\Facades\DataTables;
 
 class SiteuserController extends Controller
 {
 
-    public function index()
+    public function index(Request $request)
     {
-        $users = User::whereLevel(null)->orderBy('id' , 'DESC')->get();
-        $typeusers = Type_user::all();
         $menudashboards = Menudashboard::whereStatus(4)->get();
         $submenudashboards = Submenudashboard::whereStatus(4)->get();
+        if ($request->ajax()) {
+            $datas = DB::table('users')->LeftJoin('type_users', 'type_users.id', '=', 'users.type_id')
+                ->select('users.name as username' , 'users.id as userid' , 'users.phone as userphone'
+                    , 'users.phone_verify as userphoneverify' , 'users.status as userstatus' , 'users.created_at as usercreated' , 'type_users.title as typetitle')
+                ->where('users.level','=',null)
+                ->orderBy('users.id', 'desc')
+                ->get();
+            return Datatables::of($datas)
 
+                ->editColumn('username', function ($data) {
+                    return ($data->username);
+                })
+                ->editColumn('userphone', function ($data) {
+                    return ($data->userphone);
+                })
+                ->editColumn('typetitle', function ($data) {
+                    return ($data->typetitle);
+                })
+                ->editColumn('usercreated', function ($data) {
+                    return \Morilog\Jalali\Jalalian::forge($data->usercreated)->format(' H:i:s %Y/%m/%d');
+                })
+                ->editColumn('userphoneverify', function ($data) {
+                    if ($data->userphoneverify == "0") {
+                        return "تایید نشده";
+                    }
+                    elseif ($data->ticket_type == "1") {
+                        return "تایید شده";
+                    }
+                })
+                ->editColumn('userstatus', function ($data) {
+                    if ($data->userstatus == "0") {
+                        return "غیر فعال";
+                    }
+                    elseif ($data->userstatus == "1") {
+                        return "ثبت نام اولیه";
+                    }
+                    elseif ($data->userstatus == "2") {
+                        return "تایید مدیر";
+                    }
+                })
+                ->addIndexColumn()
+                ->make(true);
+
+            }
         return view('Admin.siteusers.all')
             ->with(compact('menudashboards'))
-            ->with(compact('submenudashboards'))
-            ->with(compact('typeusers'))
-            ->with(compact('users'));
+            ->with(compact('submenudashboards'));
     }
 
     public function edit($id)
--- resources/views/Admin/siteusers/all.blade.php
+++ resources/views/Admin/siteusers/all.blade.php
@@ -29,7 +29,20 @@
                                 </div>
 
                                 <div class="table-responsive">
-                                    <table class="table" id="example1">
+                                    <style>
+                                        table{
+                                            margin: 0 auto;
+                                            width: 100%;
+                                            clear: both;
+                                            border-collapse: collapse;
+                                            table-layout: fixed;
+                                            word-wrap:break-word;
+                                        }
+                                        .search-title{
+                                            width:-webkit-fill-available;
+                                        }
+                                    </style>
+                                    <table id="sample1" class="table table-striped table-bordered data-table">
                                         <thead>
                                         <tr>
                                             <th class="wd-10p"> ردیف </th>
@@ -44,61 +57,82 @@
                                         </tr>
                                         </thead>
                                         <tbody>
-                                        <?php $s = 1; ?>
-                                        @foreach($users as $user)
-                                            <tr class="odd gradeX">
-
-                                                <td>{{$s++}}</td>
-
-                                                <td>{{$user->name}}</td>
-                                                <td>{{$user->phone}}</td>
-                                                <td>{{jdate($user->created_at)->format('%Y/%m/%d')}}</td>
-                                                <td>
-                                                    @foreach($typeusers as $type_user)
-                                                        @if($type_user->id == $user->type_id)
-                                                            {{$type_user->title}}
-                                                        @endif
-                                                    @endforeach
-                                                </td>
-                                                <td>
-                                                    @if($user->phone_verify == 0)
-                                                        <button class="btn ripple btn-outline-info">تایید نشده</button>
-                                                    @elseif($user->phone_verify == 1)
-                                                        <button class="btn ripple btn-outline-success">تایید شده</button>
-                                                    @endif
-                                                </td>
-                                                <td>
-                                                    @if($user->status == 1)
-                                                        <button class="btn ripple btn-outline-info">ثبت نام اولیه</button>
-                                                    @elseif($user->status == 2)
-                                                        <button class="btn ripple btn-outline-success">تایید مدیر</button>
-                                                    @elseif($user->status == 0)
-                                                        <button class="btn ripple btn-outline-warning">غیر فعال</button>
-                                                    @endif
-                                                </td>
-                                                <td>
-                                                    <div class="btn-icon-list">
-                                                        <a href="{{ route('siteusers.edit' , $user->id ) }}" class="btn ripple btn-outline-info btn-icon">
-                                                            <i class="fe fe-edit-2"></i>
-                                                        </a>
-                                                    </div>
-                                                </td>
-                                                <td>
-                                                    <form action="{{ route('siteusers.destroy' , $user->id) }}" method="post">
-                                                        {{ method_field('delete') }}
-                                                        {{ csrf_field() }}
-                                                        <div class="btn-icon-list">
-                                                            <button type="submit" class="btn ripple btn-outline-danger btn-icon">
-                                                                <i class="fe fe-trash-2 "></i>
-                                                            </button>
-                                                        </div>
-                                                    </form>
-                                                </td>
-                                            </tr>
-                                        @endforeach
                                         </tbody>
                                     </table>
+
                                 </div>
+
+{{--                                <div class="table-responsive">--}}
+{{--                                    <table class="table" id="example1">--}}
+{{--                                        <thead>--}}
+{{--                                        <tr>--}}
+{{--                                            <th class="wd-10p"> ردیف </th>--}}
+{{--                                            <th class="wd-10p"> نام و نام خانوادگی </th>--}}
+{{--                                            <th class="wd-10p"> شماره موبایل </th>--}}
+{{--                                            <th class="wd-10p"> تاریخ ایجاد حساب </th>--}}
+{{--                                            <th class="wd-10p"> نوع همکاری </th>--}}
+{{--                                            <th class="wd-10p"> وضعیت شماره </th>--}}
+{{--                                            <th class="wd-10p"> وضعیت </th>--}}
+{{--                                            <th class="wd-10p"> ویرایش </th>--}}
+{{--                                            <th class="wd-10p"> حذف </th>--}}
+{{--                                        </tr>--}}
+{{--                                        </thead>--}}
+{{--                                        <tbody>--}}
+{{--                                        <?php $s = 1; ?>--}}
+{{--                                        @foreach($users as $user)--}}
+{{--                                            <tr class="odd gradeX">--}}
+
+{{--                                                <td>{{$s++}}</td>--}}
+
+{{--                                                <td>{{$user->name}}</td>--}}
+{{--                                                <td>{{$user->phone}}</td>--}}
+{{--                                                <td>{{jdate($user->created_at)->format('%Y/%m/%d')}}</td>--}}
+{{--                                                <td>--}}
+{{--                                                    @foreach($typeusers as $type_user)--}}
+{{--                                                        @if($type_user->id == $user->type_id)--}}
+{{--                                                            {{$type_user->title}}--}}
+{{--                                                        @endif--}}
+{{--                                                    @endforeach--}}
+{{--                                                </td>--}}
+{{--                                                <td>--}}
+{{--                                                    @if($user->phone_verify == 0)--}}
+{{--                                                        <button class="btn ripple btn-outline-info">تایید نشده</button>--}}
+{{--                                                    @elseif($user->phone_verify == 1)--}}
+{{--                                                        <button class="btn ripple btn-outline-success">تایید شده</button>--}}
+{{--                                                    @endif--}}
+{{--                                                </td>--}}
+{{--                                                <td>--}}
+{{--                                                    @if($user->status == 1)--}}
+{{--                                                        <button class="btn ripple btn-outline-info">ثبت نام اولیه</button>--}}
+{{--                                                    @elseif($user->status == 2)--}}
+{{--                                                        <button class="btn ripple btn-outline-success">تایید مدیر</button>--}}
+{{--                                                    @elseif($user->status == 0)--}}
+{{--                                                        <button class="btn ripple btn-outline-warning">غیر فعال</button>--}}
+{{--                                                    @endif--}}
+{{--                                                </td>--}}
+{{--                                                <td>--}}
+{{--                                                    <div class="btn-icon-list">--}}
+{{--                                                        <a href="{{ route('siteusers.edit' , $user->id ) }}" class="btn ripple btn-outline-info btn-icon">--}}
+{{--                                                            <i class="fe fe-edit-2"></i>--}}
+{{--                                                        </a>--}}
+{{--                                                    </div>--}}
+{{--                                                </td>--}}
+{{--                                                <td>--}}
+{{--                                                    <form action="{{ route('siteusers.destroy' , $user->id) }}" method="post">--}}
+{{--                                                        {{ method_field('delete') }}--}}
+{{--                                                        {{ csrf_field() }}--}}
+{{--                                                        <div class="btn-icon-list">--}}
+{{--                                                            <button type="submit" class="btn ripple btn-outline-danger btn-icon">--}}
+{{--                                                                <i class="fe fe-trash-2 "></i>--}}
+{{--                                                            </button>--}}
+{{--                                                        </div>--}}
+{{--                                                    </form>--}}
+{{--                                                </td>--}}
+{{--                                            </tr>--}}
+{{--                                        @endforeach--}}
+{{--                                        </tbody>--}}
+{{--                                    </table>--}}
+{{--                                </div>--}}
                             </div>
                         </div>
                     </div>
@@ -106,6 +140,7 @@
             </div>
         </div>
     </div>
+@endsection
 @section('end')
     <script src="{{asset('admin/assets/plugins/datatable/jquery.dataTables.min.js')}}"></script>
     <script src="{{asset('admin/assets/plugins/datatable/dataTables.bootstrap4.min.js')}}"></script>
@@ -115,5 +150,69 @@
     <script src="{{asset('admin/assets/plugins/datatable/fileexport/buttons.html5.min.js')}}"></script>
     <script src="{{asset('admin/assets/plugins/datatable/fileexport/buttons.colVis.min.js')}}"></script>
     <script src="{{asset('admin/assets/js/table-data.js')}}"></script>
+    <script>
+        // Setup - add a text input to each footer cell
+        $('#sample1 thead tr').clone(true).appendTo('#sample1 thead');
+        $('#sample1 thead tr:eq(1) th').each(function (i) {
+            var title = $(this).text();
+            $(this).html('<input class="search-title" type="text" placeholder="جستجو " />');
+
+            $('input', this).on('change change', function () {
+                if (table.column(i).search() !== this.value) {
+                    table
+                        .column(i)
+                        .search(this.value)
+                        .draw();
+                }
+            });
+        });
+
+        $('#sample1').on('processing.dt', function (e, settings, processing) {
+            if (processing) {
+                $('.preloader').show();
+            } else {
+                $('.preloader').hide();
+            }
+        });
+        var table = $('#sample1').DataTable({
+            "order": [0, 'desc'],
+            orderCellsTop: true,
+            serverSide: true,
+            scrollx: true,
+            "scrollY": "555px",
+            "scrollX": false,
+            "paging": false,
+            "ordering": false,
+            "autoWidth": false,
+            "fixedHeader": {
+                "header": false,
+                "footer": false
+            },
+            ajax: "{{ route('siteusers.index') }}",
+            columns: [
+                {data: 'userid'             , name: 'userid'},
+                {data: 'username'           , name: 'username'},
+                {data: 'userphone'          , name: 'userphone'},
+                {data: 'typetitle'          , name: 'typetitle'},
+                {data: 'usercreated'        , name: 'usercreated'},
+                {data: 'userphoneverify'    , name: 'userphoneverify'},
+                {data: 'userstatus'         , name: 'userstatus'},
+            ],
+            "columnDefs": [
+                { "width": "10px", "targets": 0 },
+                { "width": "70px", "targets": 1 },
+                { "width": "70px", "targets": 2 },
+                { "width": "70px", "targets": 3 },
+                { "width": "70px", "targets": 4 },
+                { "width": "50px", "targets": 5 },
+                { "width": "70px", "targets": 6 },
+                { "width": "70px", "targets": 7 },
+                { "width": "70px", "targets": 8 },
+            ]
+
+        });
+
+
+    </script>
 @endsection
-@endsection
+
