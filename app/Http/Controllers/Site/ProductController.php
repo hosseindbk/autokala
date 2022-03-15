d10ca2fb11827b7c1243dcf8484f1a8773af7804
hosseindbk <hosseindbk@gmail.com>
2022-03-15 17:52:28 +0330

    4.9 update product & subproduct

--- app/Http/Controllers/Site/ProductController.php
+++ app/Http/Controllers/Site/ProductController.php
@@ -42,12 +42,14 @@ class ProductController extends Controller
             ->leftJoin('product_brand_varieties', 'product_brand_varieties.product_id', '=', 'products.id')
             ->select( 'products.id as id','products.slug as slug' , 'products.image as image' , 'products.title_fa as title_fa' , 'products.title_en as title_en')
             ->orderBy('id' , 'DESC')
+            ->where('Status' , 4)
             ->paginate(16);
 
         $oldproducts = DB::table('products')
             ->leftJoin('product_brand_varieties', 'product_brand_varieties.product_id', '=', 'products.id')
             ->select( 'products.id as id','products.slug as slug' , 'products.image as image' , 'products.title_fa as title_fa' , 'products.title_en as title_en')
             ->orderBy('id')
+            ->where('Status' , 4)
             ->paginate(16);
 
         $carproducts = DB::table('car_products')
