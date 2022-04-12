2723e6d82bb61b1aabdbf2705df7e64efb800cc1
divanbeigi <hosseindbk@yahoo.com>
2022-04-12 11:16:13 +0430

    5.1 dataTable style

--- resources/views/Admin/suppliers/all.blade.php
+++ resources/views/Admin/suppliers/all.blade.php
@@ -40,6 +40,9 @@
                                             table-layout: fixed;
                                             word-wrap:break-word;
                                         }
+                                        td{
+                                            overflow-x:scroll ;
+                                        }
                                     </style>
                                     <table id="sample1" class="table table-striped table-bordered yajra-datatable">
                                         <thead>
