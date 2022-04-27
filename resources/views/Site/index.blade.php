ba284a7603a7452a36f1b3aaefd49b45c98a9d7d
divanbeigi <hosseindbk@yahoo.com>
2022-04-27 12:33:41 +0430

    5.2 test banner

--- resources/views/Site/index.blade.php
+++ resources/views/Site/index.blade.php
@@ -145,7 +145,7 @@
             </div>
 
             <div class="col-lg-4 col-md-4 col-xs-12 pl mt-1">
-                <div class="">
+                <div style="width: 100%; height: auto; margin: 0">
                     <a href="#" class="adplacement-item">
                         @foreach($left_top_slides as $slide)
                             <div class="adplacement-sponsored-box">
