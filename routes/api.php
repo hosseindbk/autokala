a434adf7fd20cc863c695d46953763fb30516ff4
hosseindbk <hosseindbk@gmail.com>
2022-05-08 17:57:01 +0430

    5.3 check api

--- routes/api.php
+++ routes/api.php
@@ -5,7 +5,7 @@ use Illuminate\Support\Facades\Route;
 Route::prefix('v1')->namespace('Api\v1')->group(function (){
 
     Route::get('/index'                             , 'IndexController@index');
-//    Route::post('/login'                            , 'UserController@login');
+    Route::post('/login'                            , 'UserController@login');
 //    Route::post('/register'                         , 'UserController@register');
 //    Route::get('/register'                          , 'UserController@getregister');
 //    Route::get('/product'                           , 'ProductController@index');
