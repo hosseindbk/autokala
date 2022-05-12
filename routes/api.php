6aaa7260f5afb0fe0308a53994de18ae4d52c129
hosseindbk <hosseindbk@gmail.com>
2022-05-12 14:20:02 +0430

    5.3 test route

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 
 Route::prefix('v1')->namespace('Api\v1')->group(function (){
+    dd('salam');
     Route::get('/index'                             , 'IndexController@index');
     Route::post('/login'                            , 'UserController@login');
     Route::post('/register'                         , 'UserController@register');
