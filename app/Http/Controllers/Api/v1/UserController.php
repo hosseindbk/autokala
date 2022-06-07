33e2c957f45c3c42263c08b771e11ee42a8e0710
divanbeigi <hosseindbk@yahoo.com>
2022-06-07 09:07:48 +0430

    5.4 edit supplier technical in profile

--- app/Http/Controllers/Api/v1/UserController.php
+++ app/Http/Controllers/Api/v1/UserController.php
@@ -213,7 +213,6 @@ class UserController extends Controller
                 ->select('markusers.id as mark_id'  ,'suppliers.id'         , 'suppliers.title'         , 'suppliers.slug'          , 'suppliers.address'   , 'suppliers.manager'   , 'suppliers.image'
                     , 'suppliers.manufacturer'      , 'suppliers.importer'  , 'suppliers.whole_seller'  , 'suppliers.retail_seller' , 'suppliers.phone'     , 'suppliers.mobile'    , 'suppliers.image2' , 'suppliers.image3'
                     , 'suppliers.website'           , 'suppliers.email'     , 'suppliers.whatsapp'      , 'suppliers.lat'           , 'suppliers.lng'       , 'suppliers.state_id'  , 'suppliers.city_id' ,'suppliers.autokala')
-                ->where('suppliers.status' , '=', 4)
                 ->where('suppliers.user_id' , '=', auth::user()->id)
                 ->get();
 
