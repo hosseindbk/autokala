27f0d0121b2fb1f011421a8e52d331423b31a9a6
divanbeigi <hosseindbk@yahoo.com>
2022-06-07 11:35:26 +0430

    5.4 edit company

--- resources/views/master-company.blade.php
+++ resources/views/master-company.blade.php
@@ -28,7 +28,7 @@
                 <div class="footer-middlebar">
                     <div class="col-lg-12 d-block pr">
                         <div class="footer-links">
-                            <div class="col-lg-3 col-md-3 col-xs-12 pr">
+                            <div class="col-lg-4 col-md-4 col-xs-12 pr">
                                 <div class="row">
                                     <section class="footer-links-col">
                                         <div class="headline-links">
@@ -41,69 +41,36 @@
                                             <a href="Tel:{{$supplier->phone}}" style="color: #716f6f;font-size: 20px" target="_blank">{{$supplier->phone}}</a> - <a href="Tel:{{$supplier->phone}}" target="_blank" style="color: #716f6f;font-size: 20px">{{$supplier->phone}}</a>
                                         </div>
                                         <div class="post-content">
-                                            <i class="fa fa-telegram" style="color: #0ab2e6"></i>
+                                            <i class="fa fa-mobile" style="color: #0ab2e6"></i>
                                             <a href="telegram:{{$supplier->mobile}}" style="color: #716f6f;font-size: 20px" target="_blank"> {{$supplier->mobile}} </a>
                                         </div>
                                         <div class="post-content">
                                             <i class="fa fa-whatsapp" style="color: #0cc745">  </i>
                                             <a href="whatsapp:{{$supplier->whatsapp}}" style="color: #716f6f;font-size: 20px" target="_blank"> {{$supplier->whatsapp}} </a>
                                         </div>
-
-{{--                                        <ul class="footer-menu-ul">--}}
-{{--                                            <li class="menu-item-type-custom">--}}
-{{--                                                <a href="tel:{{$supplier->phone}}">--}}
-{{--                                                تلفن ثابت :     {{$supplier->phone}}--}}
-{{--                                                </a>--}}
-{{--                                            </li>--}}
-{{--                                            <li class="menu-item-type-custom">--}}
-{{--                                                <a href="tel:{{$supplier->mobile}}">--}}
-{{--                                                    تلفن همراه :   {{$supplier->mobile}}--}}
-{{--                                                </a>--}}
-{{--                                            </li>--}}
-{{--                                            <li class="menu-item-type-custom">--}}
-{{--                                                <a href="tel:{{$supplier->whatsapp}}">--}}
-{{--                                                    شبکه اجتماعی :   {{$supplier->whatsapp}}--}}
-{{--                                                </a>--}}
-{{--                                            </li>--}}
-{{--                                        </ul>--}}
+                                        <div class="post-content">
+                                            <a href="whatsapp:{{$supplier->website}}" style="color: #716f6f;font-size: 20px" target="_blank"> وبسایت </a>
+                                        </div>
+                                        <div class="post-content">
+                                            <i class="fa fa-map-pin" style="color: #ff3d00"></i>
+                                            <a href="" style="color: #716f6f;font-size: 20px">استان {{$supplier->state}} شهرستان {{$supplier->city}} - {{$supplier->address}} </a>
+                                        </div>
                                     </section>
                                 </div>
                             </div>
-                            <div class="col-lg-3 col-md-3 col-xs-12 pr">
+                            <div class="col-lg-4 col-md-4 col-xs-12 pr">
                                 <div class="row">
                                     <section class="footer-links-col">
                                         <div class="headline-links">
                                             <a href="#">
-                                                آدرس
+                                                {{$supplier->title}}
                                             </a>
                                         </div>
-                                        <div class="post-content">
-                                            <i class="fa fa-map-pin" style="color: #ff3d00"></i>
-                                            <a href="" style="color: #716f6f;font-size: 20px"> {{$supplier->address}} </a>
-                                        </div>
+                                        <img src="{{asset($supplier->logo)}}" alt="{{$supplier->title}}">
                                     </section>
                                 </div>
                             </div>
-                            <div class="col-lg-3 col-md-3 col-xs-12 pr">
-                                <div class="row">
-                                    <section class="footer-links-col">
-                                        <div class="headline-links">
-                                            <a href="#">
-                                                استان و شهرستان
-                                            </a>
-                                        </div>
-                                        <div class="post-content">
-                                            <i class="fa fa-map-pin" style="color: #ff3d00"></i>
-                                            <a href="" style="color: #716f6f;font-size: 20px"> استان : {{$supplier->state}} </a>
-                                        </div>
-                                        <div class="post-content">
-                                            <i class="fa fa-map-pin" style="color: #ff3d00"></i>
-                                            <a href="" style="color: #716f6f;font-size: 20px"> شهرستان : {{$supplier->city}} </a>
-                                        </div>
-                                    </section>
-                                </div>
-                            </div>
-                            <div class="col-lg-3 col-md-3 col-xs-12 pr">
+                            <div class="col-lg-4 col-md-4 col-xs-12 pr">
                                 <div class="row">
                                     <section class="footer-links-col">
                                         <div class="headline-links">
@@ -163,6 +130,90 @@
 <script src="{{asset('site/js/vendor/jquery.ez-plus.js')}}"></script>
 <script src="{{asset('site/js/vendor/sweetalert.min.js')}}"></script>
 <script src="{{asset('site/js/main.js')}}"></script>
+<script src="{{asset('site/js/vendor/bootstrap-slider.min.js')}}"></script>
+<script src="{{asset('site/js/mapp.env.js')}}"></script>
+<script src="{{asset('site/js/mapp.min.js')}}"></script>
+<script>
+    $(document).ready(function () {
+        var crosshairIcon = {
+            iconUrl: '{{asset('site/assets/images/icon-marker.svg')}}',
+            iconSize:     [40, 50], // size of the icon
+            iconAnchor:   [20, 55], // point of the icon which will correspond to marker's location
+        };
+        var app = new Mapp({
+            element: '#app',
+            @if($supplier->lat != null && $supplier->lng != null)
+            presets: {
+                latlng: {
+                    lat: {{$supplier->lat}},
+                    lng: {{$supplier->lng}},
+                },
+                icon: crosshairIcon,
+                zoom: 20,
+                popup: {
+                    title: {
+                        i18n: 'موقعیت مکانی',
+                    },
+                    description: {
+                        i18n: 'توضیحات',
+                    },
+                    class: 'marker-class',
+                    open: false,
+                },
+            },
+            @else
+            presets: {
+                latlng: {
+                    lat: 35.73249,
+                    lng: 51.42268,
+                },
+                zoom: 14
+            },
+            @endif
+            apiKey: "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsImp0aSI6IjI0OTE4ZjYzNjQ0ZmUxNTNjMWNiY2Y1NzcyNTJlOTkzNGNkZWZhMmQyM2ZhZjBjMzdkOWViNmUzZDgyYjJmMGQ4ZjU1MDY1ZjgyY2EyNWE2In0.eyJhdWQiOiIxNTQ5NCIsImp0aSI6IjI0OTE4ZjYzNjQ0ZmUxNTNjMWNiY2Y1NzcyNTJlOTkzNGNkZWZhMmQyM2ZhZjBjMzdkOWViNmUzZDgyYjJmMGQ4ZjU1MDY1ZjgyY2EyNWE2IiwiaWF0IjoxNjMxNzc5MjQ0LCJuYmYiOjE2MzE3NzkyNDQsImV4cCI6MTYzNDQ2MTI0NCwic3ViIjoiIiwic2NvcGVzIjpbImJhc2ljIl19.VsRI2wiG_IvFVkVKXt_XnOBpzyjMIygnv6s_s81u9WVC_Z-stANinKYH_6iJPuJ3lRdAX8SdtHwYCr2DZVF2hi6WiTu-BSvMuXPb6sg0iYXgYREKQjzsWU4NPf2kOwd4q6aj1R6UOT_EA7GIrJQ5FPYDceAmeT8va1VdK6xYp-Ypstja-clURippQKEk0mDe9Z_ABYWQNAWfqUt_ubYEZrETjnDoSQHbJxJc46vxWvYmwoK1sIZ4NoXaQbRrAb0QKZ_7Lnh3H3_vHqQGMB0vJELzwSJEmiNxr_h7uIvugtRAUneAa878lOJuv03976YNjIoepK_aWhxzrP-RmE4O5A",
+        });
+        app.addLayers();
+        app.addZoomControls();
+        app.addGeolocation({
+            history: false,
+            onLoad: false,
+            onLoadCallback: function(){
+                console.log(app.states.user.latlng);
+            },
+        });
+        app.addLogo({
+            url: '{{asset('site/images/maplogo.png')}}',
+        });
+
+        @if($supplier->lat != null && $supplier->lng != null)
+
+        app.markReverseGeocode({
+            state: {
+                latlng: {
+                    lat: {{$supplier->lat}},
+                    lng: {{$supplier->lng}},
+                },
+                zoom: 14,
+                icon: crosshairIcon,
+            },
+        });
+        @endif
+    });
+</script>
+<script>
+    $('#select-all').click(function(event) {
+        if(this.checked) {
+            // Iterate each checkbox
+            $(':checkbox').each(function() {
+                this.checked = true;
+            });
+        } else {
+            $(':checkbox').each(function() {
+                this.checked = false;
+            });
+        }
+    });
+</script>
 @yield('script')
 
 
