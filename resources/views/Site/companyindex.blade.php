bb09bf1a7a81260a704c21ab763eea4bc7449beb
divanbeigi <hosseindbk@yahoo.com>
2022-06-11 09:48:32 +0430

    5.4 edit company

--- resources/views/Site/companyindex.blade.php
+++ resources/views/Site/companyindex.blade.php
@@ -12,9 +12,7 @@
                 <div class="row">
                     <div class="col-lg-12 col-md-12 col-xs-12">
                         @foreach($suppliers as $supplier)
-                            <div style="height: 200px">
-                                <img src="{{asset($supplier->banner)}}" class="d-block w-100" height="200px" alt="{{$supplier->title}}">
-                            </div>
+                            <div style="height: 10.8vmax;background-image: url({{asset($supplier->banner)}});background-size: cover;"></div>
                         @endforeach
                     </div>
                 </div>
