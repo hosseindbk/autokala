0ab4e3e2b65e64ea4d316a473fb6cd93f4c5aa71
divanbeigi <hosseindbk@yahoo.com>
2022-06-29 10:46:04 +0430

    5.5 edit offer variety

--- resources/views/Site/offerproduct.blade.php
+++ resources/views/Site/offerproduct.blade.php
@@ -279,9 +279,14 @@
                                                         </div>
                                                         <div class="col-md-4">
                                                             <div class="form-group">
-                                                                <p class="mg-b-10">تصویر اصلی قطعه</p>
-                                                                <input type="file" name="image1" @foreach($products as $product)  @if($product->image)  value="{{$product->image}}"  data-default-file="{{url($product->image)}}" @endif @endforeach class="dropify" data-height="200">
-                                                                <input type="hidden" name="image1" @foreach($products as $product)  @if($product->image)  value="{{$product->image}}"  @endif @endforeach>
+                                                                <p class="mg-b-10">{{$varity}}تصویر اصلی قطعه</p>
+                                                                @if($varity = 0 )
+                                                                    <input type="file" name="image1"    @foreach($products as $product)  @if($product->image)  value="{{$product->image}}"  data-default-file="{{url($product->image)}}" @endif @endforeach class="dropify" data-height="200">
+                                                                    <input type="hidden" name="image1"  @foreach($products as $product)  @if($product->image)  value="{{$product->image}}"  @endif @endforeach>
+                                                                @elseif($varity = 1)
+                                                                    <input type="file" name="image1"    @foreach($kalabrands as $kalabrand)  @if($kalabrand->image1)  value="{{$kalabrand->image1}}"  data-default-file="{{url($kalabrand->image1)}}" @endif @endforeach class="dropify" data-height="200">
+                                                                    <input type="hidden" name="image1"  @foreach($kalabrands as $kalabrand)  @if($kalabrand->image1)  value="{{$kalabrand->image1}}"  @endif @endforeach>
+                                                                @endif
                                                             </div>
                                                         </div>
                                                         <div class="col-md-4">
@@ -530,7 +535,7 @@
                                                         </div>
                                                         <div class="col-md-4">
                                                             <div class="form-group">
-                                                                <p class="mg-b-10">{{$varity}}تصویر قطعه</p>
+                                                                <p class="mg-b-10">{{$varity}}تصویر اصلی قطعه</p>
                                                                 @if($varity = 0 )
                                                                 <input type="file" name="image1"    @foreach($products as $product)  @if($product->image)  value="{{$product->image}}"  data-default-file="{{url($product->image)}}" @endif @endforeach class="dropify" data-height="200">
                                                                 <input type="hidden" name="image1"  @foreach($products as $product)  @if($product->image)  value="{{$product->image}}"  @endif @endforeach>
