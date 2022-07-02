767616da92e1bef167c5d3e87742b10c6b8ff876
divanbeigi <hosseindbk@yahoo.com>
2022-07-02 10:10:03 +0430

    5.6 add keywords tag

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -17,7 +17,7 @@
     <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
 
     <meta name="description" content="">
-    <meta name="keywords" content="@foreach($menus as $menu) @if(Request::segment(1) == $menu->slug) {{$menu->keyword}} @endif @endforeach">
+    <meta name="keywords" content="@foreach($menus as $menu) @if(Request::segment(1) == $menu->slug) {{Request::segment(1)}} - {{$menu->keyword}} @endif @endforeach">
     <meta name="author" content="Bestagroup">
 </head>
 
