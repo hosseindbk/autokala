a7f7f69208b330bf7a57ae41a7ca302e6746f127
divanbeigi <hosseindbk@yahoo.com>
2022-07-26 09:59:48 +0430

    5.9 edit filter market

--- app/Http/Controllers/Site/MarketController.php
+++ app/Http/Controllers/Site/MarketController.php
@@ -31,9 +31,9 @@ class MarketController extends Controller
         $buy                    = 0;
         $countState             = null;
         $users                  = User::select('id' , 'type_id')->get();
-        $selloffers             = Offer::whereStatus(4)->whereBuyorsell('sell')->latest()->state()->paginate('16');
-        $max_price              = Offer::whereStatus(4)->max('single_price');
-        $min_price              = Offer::whereStatus(4)->min('single_price');
+        $selloffers             = Offer::whereStatus(4)->whereBuyorsell('sell')->latest()->filter()->state()->paginate('16');
+        $max_price              = Offer::whereStatus(4)->filter()->max('single_price');
+        $min_price              = Offer::whereStatus(4)->filter()->min('single_price');
         $carproducts            = Car_product::whereStatus(4)->get();
         $carbrands              = Car_brand::whereStatus(4)->get();
         $carmodels              = Car_model::whereStatus(4)->get();
@@ -47,13 +47,14 @@ class MarketController extends Controller
         $filter                 = 0;
 
         $brandnames = Offer::
-              leftJoin('products', 'products.unicode', '=', 'offers.unicode_product')
+        leftJoin('products', 'products.unicode', '=', 'offers.unicode_product')
             ->leftJoin('product_brand_varieties', 'product_brand_varieties.id', '=', 'offers.brand_id')
             ->leftJoin('brands', 'brands.id', '=', 'product_brand_varieties.brand_id')
             ->select('offers.brand_id as brand_offer_id' , 'offers.id as offer_id' , 'product_brand_varieties.brand_id as brand_variety_id' , 'product_brand_varieties.product_id' ,'brands.title_fa')
             ->where('offers.status' , '=', '4')
             ->whereBuyorsell('sell')
             ->where('offers.brand_id' , '<>' , null)
+            ->filter()
             ->state()
             ->get();
 
@@ -80,99 +81,37 @@ class MarketController extends Controller
             ->with(compact('carmodels'));
     }
 
-    public function sellstate(){
-        $id                     = request('state_id');
-        if ($id == null){
-            $countState             = State::all();
-        }else{
-            $countState             = State::whereIn('id' , $id)->get();
-        }
+    public function sellfilter(){
         $menus                  = Menu::whereStatus(4)->get();
         $sell                   = 1;
         $buy                    = 0;
+        $countState             = null;
         $users                  = User::select('id' , 'type_id')->get();
-        $products               = Product::whereStatus(4)->get();
-        $brand_varietis         = Product_brand_variety::all();
-        $selloffers             = Offer::state()->whereStatus(4)->whereBuyorsell('sell')->latest()->paginate('16');
-        $max_price              = Offer::state()->whereStatus(4)->max('single_price');
-        $min_price              = Offer::state()->whereStatus(4)->min('single_price');
+        $selloffers             = Offer::whereStatus(4)->whereBuyorsell('sell')->latest()->filter()->state()->paginate('16');
+        $max_price              = Offer::whereStatus(4)->filter()->max('single_price');
+        $min_price              = Offer::whereStatus(4)->filter()->min('single_price');
         $carproducts            = Car_product::whereStatus(4)->get();
         $carbrands              = Car_brand::whereStatus(4)->get();
         $carmodels              = Car_model::whereStatus(4)->get();
         $productgroups          = Product_group::whereStatus(4)->get();
+        $products               = Product::whereStatus(4)->get();
+        $brand_varietis         = Product_brand_variety::all();
         $states                 = State::all();
         $cities                 = City::all();
         $brands                 = Brand::whereStatus(4)->get();
         $caroffers              = Car_offer::all();
         $filter                 = 0;
 
-        $brandnames = DB::table('offers')
-            ->leftJoin('products', 'products.unicode', '=', 'offers.unicode_product')
+        $brandnames = Offer::
+        leftJoin('products', 'products.unicode', '=', 'offers.unicode_product')
             ->leftJoin('product_brand_varieties', 'product_brand_varieties.id', '=', 'offers.brand_id')
             ->leftJoin('brands', 'brands.id', '=', 'product_brand_varieties.brand_id')
             ->select('offers.brand_id as brand_offer_id' , 'offers.id as offer_id' , 'product_brand_varieties.brand_id as brand_variety_id' , 'product_brand_varieties.product_id' ,'brands.title_fa')
             ->where('offers.status' , '=', '4')
             ->whereBuyorsell('sell')
             ->where('offers.brand_id' , '<>' , null)
-            ->get();
-
-        return view('Site.market')
-            ->with(compact('brandnames'))
-            ->with(compact('products'))
-            ->with(compact('brand_varietis'))
-            ->with(compact('users'))
-            ->with(compact('countState'))
-            ->with(compact('sell'))
-            ->with(compact('buy'))
-            ->with(compact('productgroups'))
-            ->with(compact('max_price'))
-            ->with(compact('min_price'))
-            ->with(compact('caroffers'))
-            ->with(compact('selloffers'))
-            ->with(compact('cities'))
-            ->with(compact('filter'))
-            ->with(compact('states'))
-            ->with(compact('brands'))
-            ->with(compact('carbrands'))
-            ->with(compact('menus'))
-            ->with(compact('carproducts'))
-            ->with(compact('carmodels'));
-    }
-
-    public function buystate(){
-        $id                     = request('state_id');
-        if ($id == null){
-            $countState             = State::all();
-        }else{
-            $countState             = State::whereIn('id' , $id)->get();
-        }
-        $menus                  = Menu::whereStatus(4)->get();
-        $sell                   = 1;
-        $buy                    = 0;
-        $products               = Product::whereStatus(4)->get();
-        $brand_varietis         = Product_brand_variety::all();
-        $users                  = User::select('id' , 'type_id')->get();
-        $selloffers             = Offer::state()->whereStatus(4)->whereBuyorsell('buy')->latest()->paginate('16');
-        $max_price              = Offer::state()->whereStatus(4)->max('single_price');
-        $min_price              = Offer::state()->whereStatus(4)->min('single_price');
-        $carproducts            = Car_product::whereStatus(4)->get();
-        $carbrands              = Car_brand::whereStatus(4)->get();
-        $carmodels              = Car_model::whereStatus(4)->get();
-        $productgroups          = Product_group::whereStatus(4)->get();
-        $states                 = State::all();
-        $cities                 = City::all();
-        $brands                 = Brand::whereStatus(4)->get();
-        $caroffers              = Car_offer::all();
-        $filter                 = 0;
-
-        $brandnames = DB::table('offers')
-            ->leftJoin('products', 'products.unicode', '=', 'offers.unicode_product')
-            ->leftJoin('product_brand_varieties', 'product_brand_varieties.id', '=', 'offers.brand_id')
-            ->leftJoin('brands', 'brands.id', '=', 'product_brand_varieties.brand_id')
-            ->select('offers.brand_id as brand_offer_id' , 'offers.id as offer_id' , 'product_brand_varieties.brand_id as brand_variety_id' , 'product_brand_varieties.product_id' ,'brands.title_fa')
-            ->where('offers.status' , '=', '4')
-            ->whereBuyorsell('buy')
-            ->where('offers.brand_id' , '<>' , null)
+            ->filter()
+            ->state()
             ->get();
 
         return view('Site.market')
@@ -206,7 +145,7 @@ class MarketController extends Controller
         $products               = Product::whereStatus(4)->get();
         $brand_varietis         = Product_brand_variety::all();
         $users                  = User::select('id' , 'type_id')->get();
-        $buyoffers              = Offer::whereStatus(4)->whereBuyorsell('buy')->latest()->state()->paginate('16');
+        $buyoffers              = Offer::whereStatus(4)->whereBuyorsell('buy')->latest()->filter()->state()->paginate('16');
         $max_price              = Offer::whereStatus(4)->max('single_price');
         $min_price              = Offer::whereStatus(4)->min('single_price');
         $carproducts            = Car_product::whereStatus(4)->get();
@@ -219,13 +158,14 @@ class MarketController extends Controller
         $caroffers              = Car_offer::all();
         $filter                 = 0;
         $brandnames = Offer::
-              leftJoin('products', 'products.unicode', '=', 'offers.unicode_product')
+        leftJoin('products', 'products.unicode', '=', 'offers.unicode_product')
             ->leftJoin('product_brand_varieties', 'product_brand_varieties.id', '=', 'offers.brand_id')
             ->leftJoin('brands', 'brands.id', '=', 'product_brand_varieties.brand_id')
             ->select('offers.brand_id as brand_offer_id' , 'offers.id as offer_id' , 'product_brand_varieties.brand_id as brand_variety_id' , 'product_brand_varieties.product_id' ,'brands.title_fa')
             ->where('offers.status' , '=', '4')
             ->whereBuyorsell('buy')
             ->where('offers.brand_id' , '<>' , null)
+            ->filter()
             ->state()
             ->get();
         return view('Site.market')
@@ -251,55 +191,36 @@ class MarketController extends Controller
             ->with(compact('carmodels'));
     }
 
-    public function marketsellfilter(){
-
-        $productgroup = request('productgroup_id');
-        if(isset($productgroup)  && $productgroup != '') {
-            $productgroup_id = Product_group::whereIn('id', $productgroup)->get();
-        }else{$productgroup_id = null;}
-
-        $carmodel = request('car_model_id');
-        if(isset($carmodel)  && $carmodel != '') {
-            $carmodel_id = Car_model::whereIn('id', $carmodel)->get();
-        }else{$carmodel_id = null;}
-
-        $brand = request('brand_id');
-        if(isset($brand)  && $brand != '') {
-            $brand_id = Brand::whereIn('id', $brand)->get();
-        }else{$brand_id = null;}
-
-        $city = request('city_id');
-        if(isset($city)  && $city != '') {
-            $city_id = City::whereIn('id', $city)->get();
-        }else{$city_id = null;}
+    public function buyfilter(){
+        $menus                  = Menu::whereStatus(4)->get();
+        $buy                    = 1;
+        $sell                   = 0;
         $countState             = null;
-        $users                  = User::select('id' , 'type_id')->get();
         $products               = Product::whereStatus(4)->get();
         $brand_varietis         = Product_brand_variety::all();
+        $users                  = User::select('id' , 'type_id')->get();
+        $buyoffers              = Offer::whereStatus(4)->whereBuyorsell('buy')->latest()->filter()->state()->paginate('16');
+        $max_price              = Offer::whereStatus(4)->max('single_price');
+        $min_price              = Offer::whereStatus(4)->min('single_price');
         $carproducts            = Car_product::whereStatus(4)->get();
-        $caroffers              = Car_offer::all();
         $carbrands              = Car_brand::whereStatus(4)->get();
-        $productgroups          = Product_group::whereStatus(4)->get();
-        $menus                  = Menu::whereStatus(4)->get();
-        $selloffers             = Offer::filter()->whereStatus(4)->whereBuyorsell('sell')->latest()->paginate('16');
-        $count                  = offer::filter()->whereStatus(4)->whereBuyorsell('sell')->count();
-        $max_price              = Offer::filter()->whereStatus(4)->whereBuyorsell('sell')->whereStatus(1)->max('price');
-        $min_price              = Offer::filter()->whereStatus(4)->whereBuyorsell('sell')->whereStatus(1)->min('price');
-        $brands                 = Brand::whereStatus(4)->get();
         $carmodels              = Car_model::whereStatus(4)->get();
+        $productgroups          = Product_group::whereStatus(4)->get();
         $states                 = State::all();
         $cities                 = City::all();
-        $filter                 =   1;
-        $buy                    =   0;
-        $sell                   =   1;
-        $brandnames = DB::table('offers')
-            ->leftJoin('products', 'products.unicode', '=', 'offers.unicode_product')
+        $brands                 = Brand::whereStatus(4)->get();
+        $caroffers              = Car_offer::all();
+        $filter                 = 0;
+        $brandnames = Offer::
+        leftJoin('products', 'products.unicode', '=', 'offers.unicode_product')
             ->leftJoin('product_brand_varieties', 'product_brand_varieties.id', '=', 'offers.brand_id')
             ->leftJoin('brands', 'brands.id', '=', 'product_brand_varieties.brand_id')
             ->select('offers.brand_id as brand_offer_id' , 'offers.id as offer_id' , 'product_brand_varieties.brand_id as brand_variety_id' , 'product_brand_varieties.product_id' ,'brands.title_fa')
             ->where('offers.status' , '=', '4')
-            ->whereBuyorsell('sell')
+            ->whereBuyorsell('buy')
             ->where('offers.brand_id' , '<>' , null)
+            ->filter()
+            ->state()
             ->get();
         return view('Site.market')
             ->with(compact('brandnames'))
@@ -309,93 +230,7 @@ class MarketController extends Controller
             ->with(compact('countState'))
             ->with(compact('buy'))
             ->with(compact('sell'))
-            ->with(compact('carmodel_id'))
-            ->with(compact('brand_id'))
-            ->with(compact('productgroup_id'))
             ->with(compact('productgroups'))
-            ->with(compact('city_id'))
-            ->with(compact('count'))
-            ->with(compact('max_price'))
-            ->with(compact('min_price'))
-            ->with(compact('caroffers'))
-            ->with(compact('selloffers'))
-            ->with(compact('cities'))
-            ->with(compact('filter'))
-            ->with(compact('states'))
-            ->with(compact('brands'))
-            ->with(compact('carbrands'))
-            ->with(compact('menus'))
-            ->with(compact('carproducts'))
-            ->with(compact('carmodels'));
-
-
-    }
-
-    public function marketbuyfilter(){
-
-        $productgroup = request('productgroup_id');
-        if(isset($productgroup)  && $productgroup != '') {
-            $productgroup_id = Product_group::whereIn('id', $productgroup)->get();
-        }else{$productgroup_id = null;}
-
-        $carmodel = request('car_model_id');
-        if(isset($carmodel)  && $carmodel != '') {
-            $carmodel_id = Car_model::whereIn('id', $carmodel)->get();
-        }else{$carmodel_id = null;}
-
-        $brand = request('brand_id');
-        if(isset($brand)  && $brand != '') {
-            $brand_id = Brand::whereIn('id', $brand)->get();
-        }else{$brand_id = null;}
-
-        $city = request('city_id');
-        if(isset($city)  && $city != '') {
-            $city_id = City::whereIn('id', $city)->get();
-        }else{$city_id = null;}
-        $countState             = null;
-        $users                  = User::select('id' , 'type_id')->get();
-        $products               = Product::whereStatus(4)->get();
-        $brand_varietis         = Product_brand_variety::all();
-        $carproducts            = Car_product::whereStatus(4)->get();
-        $caroffers              = Car_offer::all();
-        $carbrands              = Car_brand::whereStatus(4)->get();
-        $productgroups          = Product_group::whereStatus(4)->get();
-        $menus                  = Menu::whereStatus(4)->get();
-        $buyoffers              = Offer::filter()->whereStatus(4)->whereBuyorsell('buy')->latest()->paginate('16');
-        $count                  = offer::filter()->whereStatus(4)->whereBuyorsell('buy')->count();
-        $max_price              = Offer::filter()->whereStatus(4)->whereBuyorsell('buy')->whereStatus(4)->max('price');
-        $min_price              = Offer::filter()->whereStatus(4)->whereBuyorsell('buy')->whereStatus(4)->min('price');
-        $brands                 = Brand::whereStatus(4)->get();
-        $carmodels              = Car_model::whereStatus(4)->get();
-        $states                 = State::all();
-        $cities                 = City::all();
-        $filter                 =   1;
-        $buy                    =   0;
-        $sell                   =   1;
-        $brandnames = DB::table('offers')
-            ->leftJoin('products', 'products.unicode', '=', 'offers.unicode_product')
-            ->leftJoin('product_brand_varieties', 'product_brand_varieties.id', '=', 'offers.brand_id')
-            ->leftJoin('brands', 'brands.id', '=', 'product_brand_varieties.brand_id')
-            ->select('offers.brand_id as brand_offer_id' , 'offers.id as offer_id' , 'product_brand_varieties.brand_id as brand_variety_id' , 'product_brand_varieties.product_id' ,'brands.title_fa')
-            ->where('offers.status' , '=', '4')
-            ->whereBuyorsell('sell')
-            ->where('offers.brand_id' , '<>' , null)
-            ->get();
-        return view('Site.market')
-
-            ->with(compact('brandnames'))
-            ->with(compact('products'))
-            ->with(compact('brand_varietis'))
-            ->with(compact('users'))
-            ->with(compact('countState'))
-            ->with(compact('buy'))
-            ->with(compact('sell'))
-            ->with(compact('carmodel_id'))
-            ->with(compact('brand_id'))
-            ->with(compact('productgroup_id'))
-            ->with(compact('productgroups'))
-            ->with(compact('city_id'))
-            ->with(compact('count'))
             ->with(compact('max_price'))
             ->with(compact('min_price'))
             ->with(compact('caroffers'))
@@ -408,8 +243,6 @@ class MarketController extends Controller
             ->with(compact('menus'))
             ->with(compact('carproducts'))
             ->with(compact('carmodels'));
-
-
     }
 
     public function submarket($slug){
