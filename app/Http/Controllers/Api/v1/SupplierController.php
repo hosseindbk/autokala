e5ff7f4c5259f870c7df8aad29e8e057773619bf
hosseindbk <hosseindbk@gmail.com>
2022-08-20 21:32:07 +0430

    v5.9 edit technical and supplier state

--- app/Http/Controllers/Api/v1/SupplierController.php
+++ app/Http/Controllers/Api/v1/SupplierController.php
@@ -31,11 +31,11 @@ class SupplierController extends Controller
     public function subsupplier($slug){
 
         $suppliers       = Supplier::leftjoin('markusers' , 'markusers.supplier_id' , '=' , 'suppliers.id')
-            ->leftjoin('states' , 'states.id' , '=' ,'suppliers.state_id')
-            ->leftjoin('citis'  , 'citis.id'  , '=' ,'suppliers.city_id')
+            ->leftjoin('states' , 'states.id' , '=' , 'suppliers.state_id')
+            ->leftjoin('cities' , 'cities.id' , '=' , 'suppliers.city_id')
         ->select('markusers.id as mark_id'  ,'suppliers.id'         , 'suppliers.title'         , 'suppliers.slug'          , 'suppliers.address'   , 'suppliers.manager'   , 'suppliers.image'  ,'suppliers.autokala'
             , 'suppliers.manufacturer'      , 'suppliers.importer'  , 'suppliers.whole_seller'  , 'suppliers.retail_seller' , 'suppliers.phone'     , 'suppliers.mobile'    , 'suppliers.image2' , 'suppliers.image3'
-            , 'suppliers.website'           , 'suppliers.email'     , 'suppliers.whatsapp'      , 'suppliers.lat'           , 'suppliers.lng'       , 'suppliers.state_id'  , 'states.title'     , 'suppliers.city_id' , 'citis.title'  )
+            , 'suppliers.website'           , 'suppliers.email'     , 'suppliers.whatsapp'      , 'suppliers.lat'           , 'suppliers.lng'       , 'suppliers.state_id'  , 'states.title'     , 'suppliers.city_id' , 'cities.title'  )
             ->where('suppliers.status' , '=', 4)
             ->where('suppliers.slug' , '=', $slug)
             ->get();
