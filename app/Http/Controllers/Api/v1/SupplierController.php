baf8008ddeacccffe331099bd16b4538ed6cc2f8
hosseindbk <hosseindbk@gmail.com>
2022-08-20 21:35:07 +0430

    v5.9 edit technical and supplier state

--- app/Http/Controllers/Api/v1/SupplierController.php
+++ app/Http/Controllers/Api/v1/SupplierController.php
@@ -35,7 +35,7 @@ class SupplierController extends Controller
             ->leftjoin('cities' , 'cities.id' , '=' , 'suppliers.city_id')
         ->select('markusers.id as mark_id'  ,'suppliers.id'         , 'suppliers.title'         , 'suppliers.slug'          , 'suppliers.address'   , 'suppliers.manager'   , 'suppliers.image'  ,'suppliers.autokala'
             , 'suppliers.manufacturer'      , 'suppliers.importer'  , 'suppliers.whole_seller'  , 'suppliers.retail_seller' , 'suppliers.phone'     , 'suppliers.mobile'    , 'suppliers.image2' , 'suppliers.image3'
-            , 'suppliers.website'           , 'suppliers.email'     , 'suppliers.whatsapp'      , 'suppliers.lat'           , 'suppliers.lng'       , 'suppliers.state_id'  , 'states.title'     , 'suppliers.city_id' , 'cities.title'  )
+            , 'suppliers.website'           , 'suppliers.email'     , 'suppliers.whatsapp'      , 'suppliers.lat'           , 'suppliers.lng'       , 'suppliers.state_id'  , 'states.title as state_name' , 'suppliers.city_id' , 'cities.title as city_name'  )
             ->where('suppliers.status' , '=', 4)
             ->where('suppliers.slug' , '=', $slug)
             ->get();
