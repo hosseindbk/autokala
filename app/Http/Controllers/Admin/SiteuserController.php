01f762b929e0dce9d889649c069dc47f14ff8ee8
Hossein Divan Beigi <95515839+hosseindbk@users.noreply.github.com>
2022-08-28 17:43:24 +0430

    Update SiteuserController.php

--- app/Http/Controllers/Admin/SiteuserController.php
+++ app/Http/Controllers/Admin/SiteuserController.php
@@ -55,7 +55,7 @@ class SiteuserController extends Controller
                         return \Morilog\Jalali\Jalalian::forge($data->usercreated)->format('%Y/%m/%d');
                     })
                     ->editColumn('userphoneverify', function ($data) {
-                        if ($data->userphoneverify == "0") {
+                        if ($data->userphoneverify != "1") {
                             return "تایید نشده";
                         }
                         elseif ($data->userphoneverify == "1") {
