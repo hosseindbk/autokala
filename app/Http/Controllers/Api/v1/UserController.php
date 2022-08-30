d869af3abc9ab1428bc90c81cc54e8c24ee6074a
divanbeigi <hosseindbk@yahoo.com>
2022-08-30 13:43:40 +0430

    5.9 api id offer

--- app/Http/Controllers/Api/v1/UserController.php
+++ app/Http/Controllers/Api/v1/UserController.php
@@ -201,7 +201,7 @@ class UserController extends Controller
                 ->leftJoin('states', 'states.id', '=', 'offers.state_id')
                 ->leftJoin('cities', 'cities.id', '=', 'offers.city_id')
                 ->leftJoin('users', 'users.id', '=', 'offers.user_id')
-                ->select('brands.title_fa as brand' ,'offers.total as numberofsell' , 'offers.slug' , 'offers.image1 as image' , 'offers.title_offer as title' , 'states.title as state' , 'cities.title as city' , 'offers.price as wholesaleprice' , 'offers.single_price as retailprice',
+                ->select('offers.id' ,'brands.title_fa as brand' ,'offers.total as numberofsell' , 'offers.slug' , 'offers.image1 as image' , 'offers.title_offer as title' , 'states.title as state' , 'cities.title as city' , 'offers.price as wholesaleprice' , 'offers.single_price as retailprice',
                     DB::raw( '(CASE
                         WHEN users.type_id = "1" THEN "فروشگاه"
                         WHEN users.type_id = "3" THEN "شخصی"
