5a5a9bc5dc9377deb5f6359099b9bfe10875350c
Hossein Divan Beigi <95515839+hosseindbk@users.noreply.github.com>
2022-12-05 16:27:40 +0330

    v6.2 offer image

--- resources/views/Site/offerproduct.blade.php
+++ resources/views/Site/offerproduct.blade.php
@@ -281,7 +281,8 @@
                                                             <div class="form-group">
                                                                 <p class="mg-b-10">تصویر اصلی قطعه</p>
                                                                 @if($varity = 0 )
-                                                                    <input type="file" name="image1"    @foreach($products as $product)  value="{{$product->image}}"  data-default-file="{{url($product->image)}}" @endforeach class="dropify" data-height="200">
+
+                                                                    <input type="file" name="image1"    @foreach($products as $product) @dd($product->image)  value="{{$product->image}}"  data-default-file="{{url($product->image)}}" @endforeach class="dropify" data-height="200">
                                                                     <input type="hidden" name="image1"  @foreach($products as $product)  value="{{$product->image}}" @endforeach>
                                                                 @elseif($varity = 1)
                                                                     <input type="file" name="image1"    @foreach($kalabrands as $kalabrand)  @if($kalabrand->image1)  value="{{$kalabrand->image1}}"  data-default-file="{{url($kalabrand->image1)}}" @endif @endforeach class="dropify" data-height="200">
