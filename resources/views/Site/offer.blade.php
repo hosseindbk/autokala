208280707286eadfaf222803327f0484ddeaa725
Hossein Divan Beigi <95515839+hosseindbk@users.noreply.github.com>
2023-01-02 16:01:26 +0330

    v6.2 edit offer

--- resources/views/Site/offer.blade.php
+++ resources/views/Site/offer.blade.php
@@ -228,7 +228,7 @@
                                                     <div class="col-md-12">
                                                         <div class="form-group">
                                                             <p class="mg-b-10">شرح آگهی</p>
-                                                            <textarea name="description" id="editor1" placeholder=" شرح آگهی را وارد کنید">آگهی</textarea>
+                                                            <textarea name="description" id="editor1" placeholder=" شرح آگهی را وارد کنید">شرح آگهی</textarea>
                                                             <style>
                                                                 .ck-editor{
                                                                     direction: rtl;
@@ -328,6 +328,7 @@
                                                         <div class="form-group">
                                                             <p class="mg-b-10">تلفن ثابت بهمراه کد شهرستان</p>
                                                             <input type="text" name="phone" pattern="^0\d{2}\d{8}$" placeholder="02122334455" value="{{Auth::user()->phone_number}}" class="form-control text-left" />
+                                                            <p>مثل : 02122334455</p>
                                                         </div>
                                                     </div>
                                                     <div class="col-md-3">
@@ -529,7 +530,7 @@
                                                     <div class="col-md-12">
                                                         <div class="form-group">
                                                             <p class="mg-b-10">شرح آگهی</p>
-                                                            <textarea name="description" id="editor2" placeholder=" شرح آگهی را وارد کنید">آگهی</textarea>
+                                                            <textarea name="description" id="editor2" placeholder=" شرح آگهی را وارد کنید">شرح آگهی</textarea>
                                                             <style>
                                                                 .ck-editor{
                                                                     direction: rtl;
@@ -597,8 +598,10 @@
                                                     <div class="col-md-3">
                                                         <div class="form-group">
                                                             <p  class="mg-b-10">تلفن ثابت بهمراه کد شهرستان</p >
-                                                            <input type="text" name="phone" value="{{Auth::user()->phone_number}}" required pattern="^0\d{2}\d{8}$" oninvalid="this.setCustomValidity('لطفا تلفن را وارد کنید')"
+                                                            <input type="text" name="phone" value="{{Auth::user()->phone_number}}" required pattern="^0\d{2}\d{8}$" title="لطفا شماره تلفن را کامل وارد نمایید" oninvalid="this.setCustomValidity('لطفا تلفن را وارد کنید')"
                                                                    oninput="setCustomValidity('')" placeholder="02188556644" class="form-control text-left" />
+                                                            <p>مثل : 02122334455</p>
+
                                                             <input type="hidden" name="lat" class="form-control" />
                                                             <input type="hidden" name="lng" class="form-control" />
                                                         </div>
